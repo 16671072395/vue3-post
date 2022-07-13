0a0fb0f4c12fd1f6be9fd8da8c8508ea9dd41bf7
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-13 17:56:08 +0800

    添加node-polyfill-webpack-plugin插件配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: false,
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: config => {
+    const plugins = []
+    plugins.push(new NodePolyfillPlugin())
+  }
 })
