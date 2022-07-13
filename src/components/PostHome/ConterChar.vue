082ec99492baec6c85aed24dab77a0382589a5dc
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-13 17:58:01 +0800

    首页中间中国地图

--- src/components/PostHome/ConterChar.vue
+++ src/components/PostHome/ConterChar.vue
@@ -4,81 +4,204 @@
 
 <script>
 import * as echarts from "echarts";
+import { mapData } from "../../assets/mapData.js";
+
 export default {
   name: "RightTwo",
+
   mounted() {
     let myEcharts = echarts.init(this.$refs.mychart);
+    //注册地图数据
+    echarts.registerMap("ChinaMap", mapData);
+    var lineData = new Array();
+    var pointData = [
+      {
+        name: "北京", // 数据项名称，在这里指地区名称
+        value: [
+          // 数据项值
+          116.46, // 地理坐标，经度
+          39.92, // 地理坐标，纬度
+          240, // 北京地区的数值
+        ],
+      },
+      {
+        name: "陕西", // 数据项名称，在这里指地区名称
+        value: [108.95464, 34.346892, 140],
+      },
+      {
+        name: "河南", // 数据项名称，在这里指地区名称
+        value: [113.655149, 34.80912, 120],
+      },
+      {
+        name: "西藏", // 数据项名称，在这里指地区名称
+        value: [88.634825, 31.277171, 2],
+      },
+      {
+        name: "新疆", // 数据项名称，在这里指地区名称
+        value: [85.617669, 41.26232, 3],
+      },
+      {
+        name: "青海", // 数据项名称，在这里指地区名称
+        value: [96.508869, 36.014025, 10],
+      },
+      {
+        name: "甘肃", // 数据项名称，在这里指地区名称
+        value: [104.456501, 34.80912, 10],
+      },
+      {
+        name: "四川", // 数据项名称，在这里指地区名称
+        value: [102.837539, 30.515618, 14],
+      },
+      {
+        name: "云南", // 数据项名称，在这里指地区名称
+        value: [101.512933, 24.549794, 18],
+      },
+      {
+        name: "贵州", // 数据项名称，在这里指地区名称
+        value: [107.032122, 26.618626, 18],
+      },
+      {
+        name: "内蒙", // 数据项名称，在这里指地区名称
+        value: [118.659214, 44.455489, 8],
+      },
+      {
+        name: "山西", // 数据项名称，在这里指地区名称
+        value: [112.183365, 37.494395, 28],
+      },
+      {
+        name: "湖北", // 数据项名称，在这里指地区名称
+        value: [112.183365, 31.277171, 41],
+      },
+      {
+        name: "山东", // 数据项名称，在这里指地区名称
+        value: [118.438446, 36.252817, 14],
+      },
+      {
+        name: "安徽", // 数据项名称，在这里指地区名称
+        value: [116.966663, 32.532698, 26],
+      },
+      {
+        name: "江苏", // 数据项名称，在这里指地区名称
+        value: [119.174338, 33.709271, 28],
+      },
+      {
+        name: "江西", // 数据项名称，在这里指地区名称
+        value: [115.642057, 27.999098, 35],
+      },
+      {
+        name: "黑龙江", // 数据项名称，在这里指地区名称
+        value: [128.667343, 46.778951, 17],
+      },
+      {
+        name: "吉林", // 数据项名称，在这里指地区名称
+        value: [126.533257, 43.230492, 13],
+      },
+      {
+        name: "辽宁", // 数据项名称，在这里指地区名称
+        value: [123.184949, 41.567014, 12],
+      },
+      {
+        name: "浙江", // 数据项名称，在这里指地区名称
+        value: [120.020614, 28.877263, 53],
+      },
+      {
+        name: "湖南", // 数据项名称，在这里指地区名称
+        value: [111.705036, 27.442429, 36],
+      },
+      {
+        name: "广东", // 数据项名称，在这里指地区名称
+        value: [113.250409, 24.179016, 84],
+      },
+      {
+        name: "福建", // 数据项名称，在这里指地区名称
+        value: [117.886527, 25.655451, 72],
+      },
+      {
+        name: "广西", // 数据项名称，在这里指地区名称
+        value: [108.283139, 22.957987, 742],
+      },
+    ];
+    //该函数用于给画线组件数据源填充数据
+    function drawline(pointData, lineData) {
+      for (var i = 0; i < pointData.length; i++) {
+        //填充线段两点的地名
+        var point = [pointData[i].name, pointData[pointData.length - 1].name];
+        // lineData[0].push(point);
+        //填充线段两点的经纬度
+        var coords = [
+          [pointData[i].value[0], pointData[i].value[1]],
+          [108.283139, 22.957987], //广西南宁的经纬度
+        ];
+        var temp = { coords, point };
+        // lineData[1].push(coord);
+        lineData.push(temp);
+      }
+      return lineData;
+    }
+    drawline(pointData, lineData);
+    console.log(lineData);
     myEcharts.setOption({
-      backgroundColor: "#2c343c",
-      title: {
-        text: "Customized Pie",
-        left: "center",
-        top: 20,
-        textStyle: {
-          color: "#ccc",
-        },
-      },
-      tooltip: {
-        trigger: "item",
-      },
-      visualMap: {
-        show: false,
-        min: 80,
-        max: 600,
-        inRange: {
-          colorLightness: [0, 1],
+      geo: {
+        //地理坐标组件
+        type: "map",
+        map: "ChinaMap",
+        roam: true, //开启缩放和拖动
+        zoom: 1.5, //地图层级
+        center: [109.256471, 36.011222], //地图中心点
+        itemStyle: {
+          areaColor: "rgb(192, 195, 220)",
         },
       },
       series: [
         {
-          name: "Access From",
-          type: "pie",
-          radius: "55%",
-          center: ["50%", "50%"],
-          data: [
-            { value: 335, name: "Direct" },
-            { value: 310, name: "Email" },
-            { value: 274, name: "Union Ads" },
-            { value: 235, name: "Video Ads" },
-            { value: 400, name: "Search Engine" },
-          ].sort(function (a, b) {
-            return a.value - b.value;
-          }),
-          roseType: "radius",
+          type: "effectScatter", //带有涟漪特效动画的散点（气泡）图。利用动画特效可以将某
+          coordinateSystem: "geo", //使用什么坐标系geo使用地理坐标系
+          data: pointData,
           label: {
-            color: "rgba(255, 255, 255, 0.3)",
+            show: true,
+            formatter: "\n\n{b}",
           },
-          labelLine: {
-            lineStyle: {
-              color: "rgba(255, 255, 255, 0.3)",
-            },
-            smooth: 0.2,
-            length: 10,
-            length2: 20,
+          // 这个时候地图上就会有点的涟漪效果
+          rippleEffect: {
+            //涟漪特效相关配置。
+            number: 1, //波纹的数量。
+            scale: 2, //动画中波纹的最大缩放比例
           },
+          // label:{
+          // show:true
+          // },
           itemStyle: {
-            color: "#c23531",
-            shadowBlur: 200,
-            shadowColor: "rgba(0, 0, 0, 0.5)",
-          },
-          animationType: "scale",
-          animationEasing: "elasticOut",
-          animationDelay: function (idx) {
-            return Math.random() * 200;
+            color: "rgb(214, 118, 118)",
           },
         },
         {
-          name: "BackGround",
-          radius: "70%",
-          center: ["50%", "50%"],
-          data: [10, 12],
-          itemStyle: {
+          name: "",
+          type: "lines",
+          zlevel: 6,
+          lineStyle: {
+            type: "solid",
+            width: 2,
+            opacity: 0.5, //线的透明度
+            curveness: 0.1, //线的曲度
+            orient: "horizontal",
             color: "red",
-            shadowBlur: 200,
-            shadowColor: "rgba(0, 0, 0, 0.5)",
           },
+          show: true,
+          data: lineData,
+          // data: [
+          //   {
+          //     point: ["北京", "广西"],
+          //     coords: [
+          //       [116.46, 39.92],
+          //       [108.283139, 22.957987],
+          //     ],
+          //   },
+          // ],
         },
       ],
+    });
+    window.addEventListener("resize", () => {
+      myEcharts.resize();
     });
   },
 };
@@ -91,7 +214,7 @@ export default {
   /* background-color: aqua; */
   height: 8.2rem;
   width: 11.5rem;
-  /* border: 1px solid rgb(6, 6, 202); */
+  /* border: 1px solid rgb(214, 118, 118); */
   margin: 10px 10px 10px 0px;
 }
 </style>
