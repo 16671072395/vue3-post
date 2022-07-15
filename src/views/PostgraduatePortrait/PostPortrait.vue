2699931b91bd35d6f284c75e42ed6eb125c844bd
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-15 18:37:14 +0800

    研究生画像的两个二级路由页面

---
+++ src/views/PostgraduatePortrait/PostPortrait.vue
@@ -0,0 +1,14 @@
+<template>
+  <PortraitLayout></PortraitLayout>
+</template>
+
+<script>
+import PortraitLayout from "../../components/PostPortrait/PortraitLayout";
+export default {
+  name: "PostPortrait",
+  components: { PortraitLayout },
+};
+</script>
+
+<style>
+</style>
