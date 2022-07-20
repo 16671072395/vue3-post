098cf0f63b10cfa10d347582a3ec49c1f80c2860
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-20 16:59:32 +0800

    更新质量监测界面

--- src/views/QualityMonitor.vue
+++ src/views/QualityMonitor.vue
@@ -1,16 +1,55 @@
 <template>
-  <h2>QualityMonitor</h2>
+  <div>
+    <el-container>
+      <el-aside class="aside">
+        <!--竖向菜单 -->
+        <el-menu
+          active-text-color="#ffd04b"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          :default-active="activeIndex2"
+          text-color="#fff"
+          @open="handleOpen"
+          @close="handleClose"
+          :router="true"
+        >
+          <el-menu-item index="/collegenumber">
+            <span>生源质量</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main class="allHeight">
+        <router-view> </router-view>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
 import VerticalMenu from "../components/VerticalMenu";
+import CollegeNumber from "../views/QualityMonitorPage/CollegeNumber";
 export default {
   name: "QualityMonitor",
   components: {
     VerticalMenu,
+    CollegeNumber,
   },
 };
 </script>
 
-<style>
+<style scoped>
+.el-menu-vertical-demo {
+  height: 760px;
+  width: 120px;
+  background-color: #008bc2;
+}
+.aside {
+  width: 120px;
+}
+div {
+  font: size 0.625rem;
+}
+.allHeight {
+  padding: 0px 0px 0px 10px;
+}
 </style>
---
+++ src/views/QualityMonitorPage/CollegeNumber.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <HeaderChar></HeaderChar>
+  </header>
+  <section class="container">
+    <CenterChar></CenterChar>
+  </section>
+</template>
+
+<script>
+import HeaderChar from "../../components/CollegeNumber/HeaderChar";
+import CenterChar from "../../components/CollegeNumber/CenterChar";
+export default {
+  name: "CollegeNumber",
+  components: {
+    CenterChar,
+    HeaderChar,
+  },
+};
+</script>
+
+<style>
+header {
+  height: 5.25rem;
+  /* border: 1px solid rgb(6, 6, 202); */
+}
+.container {
+  height: 5.25rem;
+  width: 100%;
+}
+</style>
