5f9b1e936afd5c24f2190e7101998a9003345c3d
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-26 17:45:27 +0800

    教职工名录页面

---
+++ src/views/QualityMonitorPage/TeacherQuality/TeacherNames.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="myDiv">
+    <h2 style="text-align: center; font-family: 宋体; font-size: 24px">
+      教职工名单
+    </h2>
+    <p>
+      白琳　常侃　陈大海　陈海强　陈俊江　陈宁江　陈庆锋　陈秋莲　陈燕　陈玉梅　陈发裕
+      陈丹阳　蔡奕奕　邓珍琪　冯德志　傅文利　傅彦铭　顾春雨　甘瑜萍　耿敏明　何华光　胡强
+      胡亮青　华蓓　黄保华　黄福莹　黄汝维　黄毅然　黄昌巍　黄福艺　焦小焦　劳眷　黎相成
+      黎展荣　李娜　李陶深　李向华　李清光　李道丰　梁家荣　梁俊斌　梁正友　梁斌梅　刘峻
+      刘运毅　刘志中　刘智斌　刘嫱　刘金辉　刘潇　柳永念　陆向艳　吕品　罗远军　罗程　兰伟
+      蒙祖强　莫琳　莫国志　聂雄　欧元汉　庞承业　秦亮曦　覃华　覃团发　覃新贤　覃玉荣
+      覃希　石娟　石松　宋玲　孙友明　乔永庆　孙宇　孙翠敏　唐天兵　唐振华　唐春艳　唐印浒
+      滕金芳　田阳　武新章　王高才　王冬　王丽　王烈　王淖　王新华　吴旭　韦巍　万海斌
+      翁健高　谢敏　许嘉　许华杰　严毅　杨锋　杨林峰　杨柳　杨颖　杨丽娜　杨洋　叶进　姚海涛
+      姚怡　尹梦晓　喻昕　岳岫峪　詹锋　张锦雄　张培林　张小萍　张学军　张振荣　赵亮
+      赵志刚　赵喆　郑嘉利　钟诚　周梅
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeacherNames",
+};
+</script>
+
+<style scoped>
+.myDiv {
+  height: 500px;
+  overflow: auto;
+  width: 75%;
+  color: white;
+  background-color: #008bc2;
+  position: absolute;
+  top: 10%;
+  left: 15%;
+}
+p {
+  text-align: left;
+  line-height: 40px;
+  font-family: 宋体;
+  font-size: 21px;
+}
+</style>
