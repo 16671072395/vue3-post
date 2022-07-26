b8725c7f1afdd9bfd3786d553bb8b119dbc986cf
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-26 17:44:39 +0800

    研究生导师页面

---
+++ src/views/QualityMonitorPage/TeacherQuality/ResearchSupervisors.vue
@@ -0,0 +1,539 @@
+<template>
+  <div class="myDiv">
+    <h2 style="text-align: center; font-family: 宋体; font-size: 24px">
+      硕士生导师
+    </h2>
+    <div class="con_con" id="vsb_content">
+      <div class="v_news_content">
+        <p style="text-indent: 0em">
+          <strong
+            ><span
+              style="
+                color: white;
+                font-family: 宋体, microsoft yahei;
+                font-size: 20px;
+              "
+              >计算机科学与技术学科学术型导师（截至</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >2021</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >年6</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >月，以拼音为序排列）：</span
+            ></strong
+          >
+        </p>
+        <p style="text-indent: 0em">
+          <strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span
+              style="font-family: 宋体, microsoft yahei; font-size: 20px"
+            ></span></strong
+          ><span
+            style="
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: normal;
+              word-spacing: 0px;
+              float: none;
+              display: inline !important;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span
+              style="
+                color: white;
+                text-transform: none;
+                text-indent: 32px;
+                letter-spacing: normal;
+                font-family: 宋体, microsoft yahei;
+                font-size: 20px;
+                font-style: normal;
+                font-weight: normal;
+                word-spacing: 0px;
+                float: none;
+                display: inline !important;
+                orphans: 2;
+                widows: 2;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                -webkit-text-stroke-width: 0px;
+              "
+              >白琳，蔡毓，常侃，陈海强，陈宁江，陈庆锋，陈秋莲，陈燕，陈丹阳，傅彦铭，葛志辉，耿敏明，黄保华，黄汝维，<span
+                style="color: white; font-family: 宋体; font-size: 20px"
+                >黄昌巍，</span
+              >霍林，兰伟，黎展荣，李道丰，李清光，李陶深，梁家荣，梁俊斌，梁正友，刘峻，刘嫱，刘运毅，陆向艳，吕品，蒙祖强，欧元汉，秦亮曦，宋玲，孙宇，<span
+                style="color: white; font-family: 宋体; font-size: 20px"
+                >孙友明，</span
+              >覃华，覃团发，覃新贤，覃玉荣，唐天兵，唐振华，</span
+            ><span style="font-size: 20px"
+              ><span
+                style="
+                  color: white;
+                  text-transform: none;
+                  text-indent: 32px;
+                  letter-spacing: normal;
+                  font-family: 宋体, microsoft yahei;
+                  font-size: 20px;
+                  font-style: normal;
+                  font-weight: normal;
+                  word-spacing: 0px;
+                  float: none;
+                  display: inline !important;
+                  orphans: 2;
+                  widows: 2;
+                  font-variant-ligatures: normal;
+                  font-variant-caps: normal;
+                  -webkit-text-stroke-width: 0px;
+                "
+                >&nbsp;</span
+              ></span
+            ><span
+              style="
+                color: white;
+                text-transform: none;
+                text-indent: 32px;
+                letter-spacing: normal;
+                font-family: 宋体, microsoft yahei;
+                font-size: 20px;
+                font-style: normal;
+                font-weight: normal;
+                word-spacing: 0px;
+                float: none;
+                display: inline !important;
+                orphans: 2;
+                widows: 2;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                -webkit-text-stroke-width: 0px;
+              "
+              >万海斌，王高才，王烈，韦巍，吴旭，许华杰，许嘉，杨锋，杨丽娜，杨林峰，杨颖，叶进，尹梦晓，喻昕，张小萍，张学军，
+              张振荣，赵进创，赵亮，赵志刚，郑嘉利，钟诚，黄毅然、田阳、黄福艺</span
+            ></span
+          >
+        </p>
+        <p style="text-indent: 0em">
+          <strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              ><br />电子信息专业硕士（计算机技术领域方向）导师（截至</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >2021</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >年6</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >月，以拼音为序排列）：</span
+            ></strong
+          >
+        </p>
+        <p style="text-indent: 0em">
+          <strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span
+              style="font-family: 宋体, microsoft yahei; font-size: 20px"
+            ></span></strong
+          ><span
+            style="
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, microsoft yahei;
+              font-size: 20px;
+              font-style: normal;
+              font-weight: normal;
+              word-spacing: 0px;
+              float: none;
+              display: inline !important;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span
+              style="
+                color: white;
+                text-transform: none;
+                text-indent: 32px;
+                letter-spacing: normal;
+                font-family: 宋体, microsoft yahei;
+                font-size: 20px;
+                font-style: normal;
+                font-weight: normal;
+                word-spacing: 0px;
+                float: none;
+                display: inline !important;
+                orphans: 2;
+                widows: 2;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                -webkit-text-stroke-width: 0px;
+              "
+              >白琳，陈宁江，陈琴，陈庆锋，陈秋莲，陈燕，<span
+                style="color: white; font-family: 宋体; font-size: 20px"
+                >陈丹阳，</span
+              >傅彦铭，葛志辉，黄保华，黄汝维，霍林，兰伟，黎展荣，李道丰，李陶深，梁家荣，梁俊斌，梁正友，刘峻，陆向艳，吕品，蒙祖强，欧元汉，秦亮曦，宋玲，孙宇，覃华，覃团发，唐天兵，</span
+            ><span
+              style="
+                color: white;
+                text-transform: none;
+                text-indent: 32px;
+                letter-spacing: normal;
+                font-family: 宋体, microsoft yahei;
+                font-size: 20px;
+                font-style: normal;
+                font-weight: normal;
+                word-spacing: 0px;
+                float: none;
+                display: inline !important;
+                orphans: 2;
+                widows: 2;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                -webkit-text-stroke-width: 0px;
+              "
+              >&nbsp;</span
+            ><span
+              style="
+                color: white;
+                text-transform: none;
+                text-indent: 32px;
+                letter-spacing: normal;
+                font-family: 宋体, microsoft yahei;
+                font-size: 20px;
+                font-style: normal;
+                font-weight: normal;
+                word-spacing: 0px;
+                float: none;
+                display: inline !important;
+                orphans: 2;
+                widows: 2;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                -webkit-text-stroke-width: 0px;
+              "
+              >王高才，吴旭，许华杰，许嘉，杨锋，杨丽娜，杨林峰，杨颖，叶进，尹梦晓，喻昕，张小萍，赵亮，赵志刚，钟诚，黄毅然、田阳</span
+            ></span
+          >
+        </p>
+        <p style="text-indent: 0em">
+          <strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              ><br />电子信息专业硕士（电子与通信工程领域方向）导师（截至</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >2021</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >年6</span
+            ></strong
+          ><strong
+            style="
+              margin: 0px;
+              padding: 0px;
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, 宋体, arial, sans-serif;
+              font-size: medium;
+              font-style: normal;
+              font-weight: 700;
+              word-spacing: 0px;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            ><span style="font-family: 宋体, microsoft yahei; font-size: 20px"
+              >月，以拼音为序排列）：</span
+            ></strong
+          >
+          &nbsp;
+        </p>
+        <p style="text-indent: 0em">
+          <span
+            style="
+              color: white;
+              text-transform: none;
+              text-indent: 32px;
+              letter-spacing: normal;
+              font-family: 宋体, microsoft yahei;
+              font-size: 20px;
+              font-style: normal;
+              font-weight: normal;
+              word-spacing: 0px;
+              float: none;
+              display: inline !important;
+              orphans: 2;
+              widows: 2;
+              font-variant-ligatures: normal;
+              font-variant-caps: normal;
+              -webkit-text-stroke-width: 0px;
+            "
+            >常侃，陈海强，耿敏明，黄昌巍、李清光，刘嫱，刘运毅，覃新贤，覃玉荣，孙友明，唐振华，万海斌，王烈，韦巍，张学军，张振荣，郑嘉利，<span
+              style="color: white; font-family: 宋体; font-size: 20px"
+              >赵进创、黄福艺</span
+            ></span
+          >
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResearchSupervisors",
+};
+</script>
+
+<style scoped>
+.myDiv {
+  height: 75%;
+  overflow: auto;
+  width: 75%;
+  color: white;
+  background-color: #008bc2;
+  position: absolute;
+  top: 10%;
+  left: 15%;
+}
+</style>
