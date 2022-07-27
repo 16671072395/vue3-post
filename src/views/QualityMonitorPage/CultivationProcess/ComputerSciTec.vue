94eabaa9b63612140870122453b4c81a06c242a4
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-27 22:44:43 +0800

    培养过程--计算机科学与技术界面

---
+++ src/views/QualityMonitorPage/CultivationProcess/ComputerSciTec.vue
@@ -0,0 +1,464 @@
+<template>
+  <div class="myDiv">
+    <div class="con_con" id="vsb_content">
+      <div class="v_news_content">
+        <div class="WordSection1" style="vsb_temp: 15.6pt">
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 77pt">
+            <span style="font-family: 方正小标宋_GBK; font-size: 22pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              计算机科学与技术专业介绍</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; vsb_temp-mode: char">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >一、基本情况
+              </span></strong
+            ><span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >计算机科学与技术专业创建于<span lang="EN-US">1979</span
+              >年。本专业自<span lang="EN-US">1996</span
+              >年以来一直是广西大学重点建设专业之一，在<span lang="EN-US"
+                >2005</span
+              >年广西教育厅组织的专业评估中名列广西计算机科学与技术专业第一名，进入自治区级精品专业的建设行列。于<span
+                lang="EN-US"
+                >2005</span
+              >年进入自治区级精品专业的建设行列；<span lang="EN-US">2011</span
+              >年列入“广西高等学校特色专业及课程一体化建设项目”优势专业；<span
+                lang="EN-US"
+                >2014</span
+              >年，列入广西高等学校优势特色专业建设项目；<span lang="EN-US"
+                >2019</span
+              >年列入国家一流本科专业建设点。专业拥有自治区级精品资源共享课<span
+                lang="EN-US"
+                >3</span
+              >门，自治区级一流本科课程<span lang="EN-US">3</span
+              >门。学院拥有<span lang="EN-US">1</span
+              >个广西自治区级重点实验室、<span lang="EN-US">1</span
+              >个广西工程技术研究中心、<span lang="EN-US">2</span
+              >个广西高校重点实验室、<span lang="EN-US">1</span
+              >个国家高性能计算中心南宁分中心。学院专业建设有云计算与大数据实验室、人工智能技术实验室、机器人与无人机实验室、网络技术实验室、信息安全实验室、图形图像与虚拟现实技术实验室、物联网实验室等<span
+                lang="EN-US"
+                >10</span
+              >个本科教学实验室，实验设备总值<span lang="EN-US">1531</span
+              >余万元，教学科研实验室面积<span lang="EN-US">2868.4</span
+              >平方米。专业师资力量雄厚，拥有教育部“新世纪优秀人才支持计划”人选<span
+                lang="EN-US"
+                >1</span
+              >人、广西“新世纪十百千人才工程”第二层次人选<span lang="EN-US"
+                >2</span
+              >人、广西高层次人才<span lang="EN-US">1</span
+              >人、广西八桂青年学者<span lang="EN-US">1</span
+              >人、广西高校引进海外高层次人才 “百人计划”<span lang="EN-US"
+                >1</span
+              >人、广西高校优秀人才计划人选<span lang="EN-US">1</span
+              >人等。目前共有专任教师<span lang="EN-US">25</span
+              >人，其中正高<span lang="EN-US">7</span>人，副高<span lang="EN-US"
+                >11</span
+              >人，中级职称<span lang="EN-US">7</span
+              >人，具有博士学位的教师<span lang="EN-US">21</span
+              >人，占本系教师人数的<span lang="EN-US">83%</span>。学院现有<span
+                lang="EN-US"
+                >1</span
+              >个一级学科学术硕士学位授权点、<span lang="EN-US">2</span
+              >个专业硕士领域授权点。本专业形成了“基础和工程实践并重，强化创新意识，为地方经济建设服务”的办学特色和优势。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; margin-top: 7.8pt; vsb_temp-mode: char"
+          >
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >二、培养目标 （出自计算机科学与技术专业<span lang="EN-US"
+                  >2020</span
+                >版培养计划）</span
+              ></strong
+            ><span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;
+            </span>
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >立足广西，服务全国，辐射东盟，面向世界，培养德智体美劳全面发展，具备计算机科学研究、工程实践开发和应用服务能力，能够综合运用计算机科学理论知识、方法和技术解决计算机技术及交叉领域的复杂工程问题，在计算机及相关领域从事系统开发、技术应用、系统集成、科学研究等工作，在团队担任技术骨干或管理者，具有社会责任感、法治意识、创新精神、实践能力和国际视野的计算机专业高素质复合型人才。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >本专业毕业生工作后五年左右，应达到以下五个方面的目标：</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >1</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、具有良好的社会责任感、法治意识和职业道德，能在工作过程中多视角、全面考虑问题，体现对职业、社会、环境的责任。
+            </span>
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >2</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、具有创新精神和实践能力，能够综合运用计算机学科基础理论、专业知识和工程技术解决信息技术及其交叉领域的复杂工程问题，进行科学研究、技术开发、组织管理和应用服务相关工作。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >3</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、能紧跟计算机科学与技术领域的发展前沿，了解本专业的发展现状和趋势，掌握科学研究和工程应用方法，具有国际交流能力和国际视野。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >4</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、具有很强的组织管理与协调能力，能够组织制定工作计划并有效实施，并能就计算机领域的复杂工程问题与业界同行及社会公众进行有效沟通和交流。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >5</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、具有终身学习能力，具备较强的自我获取知识、创新创业和跨文化交流与合作的能力，能够适应科学技术与社会的发展需求。
+            </span>
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; margin-top: 7.8pt; vsb_temp-mode: char"
+          >
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >三、毕业要求（出自计算机科学与技术专业<span lang="EN-US"
+                  >2020</span
+                >版培养计划）
+              </span></strong
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >1</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、工程知识：
+              能够将数学、自然科学、工程基础和专业知识用于解决计算机及相关领域的复杂工程问题。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >2</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、问题分析：
+              能够应用数学、自然科学和工程科学的基本原理，识别、表达、并通过文献研究分析计算机及相关领域的复杂工程问题，以找到解决问题的思路。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >3</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、设计<span lang="EN-US">/</span>开发解决方案：
+              能够设计针对计算机领域复杂工程问题的解决方案，设计满足特定需求的计算机系统、软硬件或相关产品，
+              并能够在设计环节中体现创新意识，考虑社会、健康、安全、法律、文化以及环境等因素。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >4</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、研究：
+              能够基于科学原理并采用科学方法对计算机领域的复杂工程问题进行研究，包括设计实验、分析与解释数据，并通过信息综合验证方案的可行性和有效性。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span>5</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、使用现代工具：
+              能够针对复杂工程问题，开发、选择与使用恰当的技术、资源、现代工程工具和信息技术工具，包括对复杂工程问题的预测与模拟，并能够理解其局限性。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >6</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、工程与社会：
+              能够基于工程相关背景知识进行合理分析，评价计算机科学与技术专业工程实践和复杂工程问题解决方案对社会、健康、
+              安全、法律以及文化的影响，并理解应承担的责任。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >7</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、环境和社会可持续发展：
+              能够理解和评价针对计算机领域复杂工程问题的工程实践对环境、社会可持续发展的影响。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >8</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、职业规范：
+              具有人文社会科学素养、社会责任感，能够在工程实践中理解并遵守工程职业道德和规范，履行责任。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >9</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、个人和团队：
+              能够在多学科背景下的团队中承担个体、团队成员以及负责人的角色。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >10</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >、沟通：
+              能够就计算机领域复杂工程问题与业界同行及社会公众进行有效沟通和交流，包括撰写卷面报告、设计文档、编写代码和口头陈述等。能够在跨文化背景下进行沟通和交流，具备一定的国际视野。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span
+              lang="EN-US"
+              style="font-family: 'Cambria Math', serif; font-size: 16pt"
+              >11</span
+            ><span style="font-family: 仿宋; font-size: 16pt">、</span
+            ><span style="font-family: 仿宋; font-size: 16pt">项目管理：</span
+            ><span style="font-family: 仿宋; font-size: 16pt">
+              能理解并掌握工程管理原理与经济决策方法，并能在多学科环境下应用。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span
+              lang="EN-US"
+              style="font-family: 'Cambria Math', serif; font-size: 16pt"
+              >12</span
+            ><span style="font-family: 仿宋; font-size: 16pt">、</span
+            ><span style="font-family: 仿宋; font-size: 16pt">终身学习：</span
+            ><span style="font-family: 仿宋; font-size: 16pt">
+              具有自主学习和终身学习的意识，不断学习和适应发展的能力。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; margin-top: 7.8pt; vsb_temp-mode: char"
+          >
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >四、主要课程</span
+              ></strong
+            ><span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </span>
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >基础课程和专业基础课程：政治理论课、体育课、大学英语、高等数学、大学物理、线性代数、概率论与数理统计、计算科学导论、程序设计基础、离散数学、数据结构、面向对象程序设计、数字电路与逻辑设计、计算机组成原理、计算机网络原理、数据库原理、算法设计与分析（全英语课）等。<span
+                lang="EN-US"
+                >&nbsp;
+              </span></span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32.15pt; vsb_temp-mode: char"
+          >
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >专业核心课：</span
+              ></strong
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >代数结构与初等数论、软件工程、计算机系统结构、编译原理、操作系统。<span
+                lang="EN-US"
+                >&nbsp;
+              </span></span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32.15pt; vsb_temp-mode: char"
+          >
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >专业限定选修课：</span
+              ></strong
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >工程管理与工程伦理、算法设计与分析（全英语课程）、汇编程序设计、应用密码学和网络安全。<span
+                lang="EN-US"
+                >&nbsp;
+              </span></span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32.15pt; vsb_temp-mode: char"
+          >
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >专业选修课：</span
+              ></strong
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >计算机图形学、人工智能原理、计算方法、模式识别与图像处理（全英语课）、面向对象程序设计、数据挖掘技术、大数据技术（全英语课）、软件测试技术、分布式计算机系统、云计算技术、高性能计算技术、<span
+                lang="EN-US"
+                >.Net</span
+              >软件开发技术、软件测试技术、移动应用编程、嵌入式技术、微机原理与接口技术等。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="line-height: 30pt; text-indent: 32pt; vsb_temp-mode: char"
+          >
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >（引用数据截止<span lang="EN-US">2022</span>年<span lang="EN-US"
+                >6</span
+              >月）</span
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComputerSciTec",
+  mounted() {
+    // var url = require("../../../assets/title.docx");
+    // this.title = this.readFile(url);
+  },
+  methods: {
+    readFile(filePath) {
+      // 创建一个新的xhr对象
+      let xhr = null;
+      if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+      } else {
+        // eslint-disable-next-line
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      const okStatus = document.location.protocol === "file" ? 0 : 200;
+      xhr.open("GET", filePath, false);
+      xhr.overrideMimeType("text/html;charset=utf-8");
+      xhr.send(null);
+      return xhr.status === okStatus ? xhr.responseText : null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.myDiv {
+  height: 760px;
+  overflow: auto;
+  width: 100%;
+  color: white;
+  background-color: #008bc2;
+  /* position: absolute;
+  top: 10%;
+  left: 15%; */
+}
+p {
+  text-align: left;
+  line-height: 40px;
+  font-family: 宋体;
+  font-size: 21px;
+}
+.con_con {
+  padding: 10px 20px 10px 20px;
+}
+p.MsoNormal,
+li.MsoNormal,
+div.MsoNormal {
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  text-align: justify;
+  text-justify: inter-ideograph;
+  font-size: 10.5pt;
+  font-family: "Calibri", sans-serif;
+}
+p.MsoHeader,
+li.MsoHeader,
+div.MsoHeader {
+  /* mso-style-link: "页眉 Char"; */
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  text-align: center;
+  /* vsb_temp-mode: char; */
+  border: none;
+  padding: 0cm;
+  font-size: 9pt;
+  font-family: "Calibri", sans-serif;
+}
+p.MsoFooter,
+li.MsoFooter,
+div.MsoFooter {
+  /* mso-style-link: "页脚 Char"; */
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  /* vsb_temp-mode: char; */
+  font-size: 9pt;
+  font-family: "Calibri", sans-serif;
+}
+
+.MsoChpDefault {
+  font-family: "Calibri", sans-serif;
+} /* Page Definitions */
+@page WordSection1 {
+  size: 595.3pt 841.9pt;
+  margin: 72pt 90pt 72pt 90pt;
+  vsb_temp: 15.6pt;
+}
+</style>
