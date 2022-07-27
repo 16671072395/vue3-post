bc21f4d2d17dbaf25b5c13cdca51ce9735ba94fc
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-27 22:45:21 +0800

    培养过程--信息安全界面

---
+++ src/views/QualityMonitorPage/CultivationProcess/InformationSecurity.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="myDiv">
+    <div class="con_con" id="vsb_content">
+      <div class="v_news_content">
+        <div class="WordSection1" style="vsb_temp: 15.6pt">
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 110pt">
+            <span style="font-family: 方正小标宋_GBK; font-size: 22pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              信息安全专业介绍</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >一、 基本情况</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >没有网络安全，就没有国家安全。目前，我国掌握网络信息安全技术的人才奇缺。<span
+                lang="EN-US"
+                >2015</span
+              >年<span lang="EN-US">6</span
+              >月，为实施国家安全战略，加快网络空间安全高层次人才培养，国务院学位委员会决定在“工学”门类下增设“网络空间安全”一级学科。广西大学早在<span
+                lang="EN-US"
+                >2006</span
+              >年就获得国家批准招收信息安全专业本科生，十多年来持续为国家培养信息安全、网络空间安全高级专业人才。广西大学信息安全专业入选省级一流专业建设点，建设有广西普通本科高校示范性现代产业学院<span
+                lang="EN-US"
+                >——</span
+              >信息安全产业学院。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >学院拥有<span lang="EN-US">1</span
+              >个广西自治区级重点实验室、<span lang="EN-US">1</span
+              >个广西工程技术研究中心、<span lang="EN-US">2</span
+              >个广西高校重点实验室、<span lang="EN-US">1</span
+              >个国家高性能计算中心南宁分中心。学院专业建设有信息安全实验室、网络技术实验室、云计算与大数据实验室、人工智能技术实验室、机器人与无人机实验室、与图形图像与虚拟现实技术实验室、物联网实验室等<span
+                lang="EN-US"
+                >10</span
+              >个本科教学实验室，实验设备总值<span lang="EN-US">1531</span
+              >余万元，教学科研实验室面积<span lang="EN-US">2868.4</span
+              >平方米。本专业师资力量雄厚，拥有教授<span lang="EN-US">5</span
+              >人、副教授<span lang="EN-US">6</span>人、高级工程师<span
+                lang="EN-US"
+                >1</span
+              >人、讲师<span lang="EN-US">2</span>人、工程师<span lang="EN-US"
+                >1</span
+              >人，具有博士学位的教师<span lang="EN-US">12</span
+              >人，专任教师全部拥有博士硕士学位。学院现有<span lang="EN-US"
+                >1</span
+              >个一级学科学术硕士学位授权点、<span lang="EN-US">2</span
+              >个专业硕士领域授权点。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >二、 培养目标</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >本专业培养德、智、体、美、劳全面发展，系统掌握信息安全的基本理论和关键技术，具有较强的计算机信息安全科学研究、技术开发、工程实践能力，能够综合运用信息安全理论、方法和技术解决信息安全及其相关领域的复杂工程问题，在信息安全领域从事研究、产品研发、工程设计与施工、安全运维、安全检测与取证以及信息安全教学等工作，具有可持续发展和社会适应能力的信息安全专门人才。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >三、培养规格与培养途径</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >本专业修业年限为四年，学生在修完教学计划所规定的全部课程并考试合格后，获得本科毕业证书，在满足规定的条件下将被授予工学学士学位。专业采用计算机大类专业招生、专业培养加导师指导的培养模式。通过大类招生，学生在大一和大二两学年的学习中，信息安全、计算机科学与技术<span
+                lang="EN-US"
+                >2</span
+              >个专业的学生一体化培养，为后两年分专业的学习准备坚实宽广的计算机基础理论知识与技术。从大三分专业开始直到毕业，在信息安全系（专业）组织管理下，信息安全专业每个学生都选定导师，在导师指导下进行计算机信息安全理论知识和技术学习，并通过有指导的综合性信息安全系统设计、创新创业实践、科研和工程项目等，让学生早做科研、早进实验室、早进科研团队，进行计算机信息安全技术实践和创新能力培养。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >四、主要课程</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >本专业学生除了学习政治理论课、体育课、高等数学、线性代数、概率论与数理统计、大学物理、英语等公共基础课外，主要课程还包括离散数学、程序设计基础、数据结构、计算机组成原理、计算机网络原理、数据库原理、信息安全导论、信息安全数学基础、密码学、编译原理、信息安全工程管理与伦理、软件安全、信息内容安全、网络与通信安全（全英）、操作系统原理及安全、应用系统安全、逆向工程、信号与系统、取证技术、可靠性技术（双语）、通信原理、嵌入式系统安全、<span
+                lang="EN-US"
+                >EDA</span
+              >技术应用、云计算与物联网安全、人工智能安全等。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >五、实施效果</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >本专业在高度重视计算机与信息安全理论知识的学习基础上，还特别注重计算机信息安全实践能力的培养，安排有计算机与信息安全课程设计、大型软件设计、信息安全实践一到四级、毕业设计、信息安全学科专业竞赛、大学生创新计划项目等多种实践环节。通过系统的理论学习和实践锻炼，本专业学生毕业后主要进入高等院校、科研院所、党政机关、军队、武警、公安、法院、检察院、司法部门、安全部门、工商系统、税务系统、海关、银行、保险、民航、电信、交通、电力、邮政、事业单位、公司和企业等，主要从事计算机与网络信息安全技术的研究、设计开发、管理、教学和应用等工作。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span style="font-family: 仿宋; font-size: 21px"
+                >（引用数据截止<span lang="EN-US">2022</span>年<span
+                  lang="EN-US"
+                  >6</span
+                >月）</span
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InformationSecurity",
+};
+</script>
+
+<style scoped>
+.myDiv {
+  height: 760px;
+  overflow: auto;
+  width: 100%;
+  color: white;
+  background-color: #008bc2;
+  /* position: absolute;
+  top: 10%;
+  left: 15%; */
+}
+p {
+  text-align: left;
+  line-height: 40px;
+  font-family: 宋体;
+  font-size: 21px;
+}
+.con_con {
+  padding: 10px 20px 10px 20px;
+}
+p.MsoNormal,
+li.MsoNormal,
+div.MsoNormal {
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  text-align: justify;
+  text-justify: inter-ideograph;
+  font-size: 10.5pt;
+  font-family: "Calibri", sans-serif;
+}
+p.MsoHeader,
+li.MsoHeader,
+div.MsoHeader {
+  /* mso-style-link: "页眉 Char"; */
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  text-align: center;
+  /* vsb_temp-mode: char; */
+  border: none;
+  padding: 0cm;
+  font-size: 9pt;
+  font-family: "Calibri", sans-serif;
+}
+p.MsoFooter,
+li.MsoFooter,
+div.MsoFooter {
+  /* mso-style-link: "页脚 Char"; */
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  /* vsb_temp-mode: char; */
+  font-size: 9pt;
+  font-family: "Calibri", sans-serif;
+}
+
+.MsoChpDefault {
+  font-family: "Calibri", sans-serif;
+} /* Page Definitions */
+@page WordSection1 {
+  size: 595.3pt 841.9pt;
+  margin: 72pt 90pt 72pt 90pt;
+  vsb_temp: 15.6pt;
+}
+</style>
