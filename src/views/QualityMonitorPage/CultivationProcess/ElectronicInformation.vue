7ed0d55fc119a814f3b1a15d45a0075c8434f3a6
16671072395 <84792121+16671072395@users.noreply.github.com>
2022-07-27 22:45:01 +0800

    培养过程--电子信息界面

---
+++ src/views/QualityMonitorPage/CultivationProcess/ElectronicInformation.vue
@@ -0,0 +1,362 @@
+<template>
+  <div class="myDiv">
+    <div class="con_con" id="vsb_content">
+      <div class="v_news_content">
+        <div class="WordSection1" style="vsb_temp: 15.6pt">
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 99pt">
+            <span style="font-family: 方正小标宋_GBK; font-size: 22pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              电子信息工程专业介绍</span
+            >
+          </p>
+           <p class="MsoNormal" style="line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >一、 基本情况</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >广西大学电子信息工程专业是国家级一流本科专业建设点，是在广西大学原无线电、电子工程、应用电子技术专业的基础上发展起来的老牌专业。<span
+                lang="EN-US"
+                >1971</span
+              >年广西大学设置无线电技术专业，首次招生。<span lang="EN-US"
+                >1978</span
+              >年春季招收第一届本科生。<span lang="EN-US">1994</span
+              >年更名为电子工程专业，<span lang="EN-US">1998</span
+              >年专业拓宽为电子信息工程专业，专业学制为四年，授予工学学士学位。本学科专业从<span
+                lang="EN-US"
+                >1996</span
+              >年开始招收通信与信息处理系统方向的硕士研究生。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >经过四十多年的教学实践，本校在电子与信息工程专业本科教学和人才培养方面积累了比较丰富的教学实践经验，形成了比较完整的教学管理体系，并在学科专业建设、教材建设和实验室建设等方面取得了较好的成绩。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >本学科专业是广西重点发展的千亿元电子信息产业的重要支撑学科专业，主要立足广西、面向全国、辐射东盟及“一带一路”信息化工程人才培养，培养德智体美劳全面发展的具有系统的、高水平的电子信息专业知识和解决复杂工程问题能力的复合型高素质人才。本专业是广西高校中设立和招生最早的电子本科专业。<span
+                lang="EN-US"
+                >2014</span
+              >年，本专业获批为广西高校优势特色本科专业，计算机与电子信息专业群列入广西大学优势特色专业群建设，同年获“电子与通信工程”专业硕士领域授权点，<span
+                lang="EN-US"
+                >2018</span
+              >年获批“信息与通信工程”一级学科学硕点，<span lang="EN-US"
+                >2021</span
+              >年获批国家级一流本科专业建设点。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >电子信息工程专业师资队伍结构合理，综合素质较高。专业现有专业教师<span
+                lang="EN-US"
+                >12</span
+              >人，其中教授<span lang="EN-US">4</span>人、副教授<span
+                lang="EN-US"
+                >6</span
+              >人，博士学位<span lang="EN-US">8</span
+              >人，拥有广西高校优秀人才计划人选<span lang="EN-US">2</span
+              >人，青年学术骨干<span lang="EN-US">1</span>人，留学回国人员<span
+                lang="EN-US"
+                >2</span
+              >人等专业人才。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >电子信息工程专业教学设施完善，实验设备先进。本专业现有生物信息技术实验室、通信技术实验室、高频技术实验室、数字电路实验室、虚拟仿真实验室等<span
+                lang="EN-US"
+                >5</span
+              >个实验室，生均教学仪器设备<span lang="EN-US">2</span
+              >万元。此外，还拥有多个比较稳定和完善的校外实习基地。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >二、 培养目标</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >电子信息工程专业旨在培养学生德、智、体、美、劳全面发展，积极主动，富有责任心，善于沟通，有团队协作精神；掌握信息传输和处理的基本理论和知识，掌握信息获取和应用的核心技术；培养具有基础理论扎实，理论实践并重，专业口径宽，适应性强，发展后劲足，综合素质高，掌握电子信息系统研究、设计、制造、应用和开发的高级工程技术型人才。学生具有批判性和创造性思维，能从工程原理出发，勇于挑战工作中的工程和非工程问题，并运用分析性的思维方式，综合信息，做出合理判断，提出创新的解决方案。学生毕业后能在信息处理、信息传输、通信网络、无线通信、计算机通信、信息系统等相关领域从事科学研究、系统设计、应用研发、集成和管理等工作，也可继续攻读信息与通信工程、电子科学与技术、计算机科学与技术等相关学科专业的研究生。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >三、培养规格与培养途径</span
+              ></strong
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >思想政治和德育方面：热爱社会主义祖国，拥护中国共产党的领导，努力学习马列主义、毛泽东思想、邓小平理论、<span
+                lang="EN-US"
+                >“</span
+              >三个代表<span lang="EN-US">”</span
+              >主要思想、科学发展观、习近平新时代中国特色社会主义思想；积极参加社会实践，愿意为社会主义现代化建设服务；热爱科学事业养成良好学风，理论联系实际，具有艰苦求实、善于合作、勇于创新的科学精神；具有良好的思想品德修养和心理素质，遵守纪律，养成良好的体育锻炼和卫生习惯，能够与广泛的听众进行有效的沟通与交流。能够在工程实践中遵循工程职业道德和规范，履行专业职责；能够综合信息，在考虑全球、经济、环境和社会影响等诸多因素的情况下，做出合理有效的判断。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >专业业务方面：本专业学生主要学习信号的获取与处理、电子、通信设备与信息系统等方面的专业知识，受到电子与信息工程实践的基本训练，具备设计、开发、应用和集成电子设备和信息系统的基本能力。总学分数至少达到<span
+                lang="EN-US"
+                >150</span
+              >，其中包括必修课<span lang="EN-US">126</span>学分。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >毕业生应获得以下几方面的知识和能力：</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >①</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >掌握电子电路的基本理论和实验技术，能够运用工程、基础科学和数学原理来识别、制定和解决复杂工程问题。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >②</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >掌握信息获取、处理的基本理论和应用的一般方法，具有设计、应用及计算机模拟信息系统的基本能力；能够应用工程设计理念设计出满足特定需求的解决方案，并在设计过程中考虑到公共健康、安全、福利，以及全球、文化、社会、环境和经济因素。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >②</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >能够掌握高等数学的知识及其应用，如微分方程、线性代数和复变函数的知识与应用。能够掌握概率论和数理统计的知识及其应用，掌握微积分、基础科学、工程科学以及计算机科学的知识，并应用这些知识来分析、设计复杂的信息工程系统。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >④</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >了解信息产业的基本方针、政策和法规；能够与广泛的听众进行有效的沟通与交流。能够在团队项目中展现领导力、创建协作包容的工作环境、设立目标、制定计划并实现目标。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >⑤</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >掌握文献检索、资料查询的基本方法，具有一定的科学研究和实际工作能力；</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >⑥</span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >能够完成自主学习，使用适当的学习策略来获取并应用新的知识。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span style="font-family: 仿宋; font-size: 16pt">体育方面：</span>
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >了解体育运动的的基本知识，掌握锻炼身体的基本方法与技能，养成科学锻炼身体的习惯，达到大学生体育合格标准。了解心理健康的知识与标准，掌握增进身心健康的方法。具有强健的体质和良好的心理素质。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span style="font-family: 仿宋; font-size: 16pt">四、主要课程</span>
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >基础课程包括：政治理论课、体育课、大学英语、高等数学、大学物理、线性代数、电路分析基础、数字电路、程序设计基础、专业英语等。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >专业课程分为学科类核心课程、专业核心课程、专业限选课程、专业选修课程、专业实践必修课、专业实践选修课。课程体系由电路类、信号类、通信类、磁波类和计算机类等五个模块组成。核心课程包括：电路分析基础、数字电路与逻辑设计、模拟电子线路、通信电子线路、电磁场与电磁波、程序设计基础、微机原理与单片机应用、信号与系统、数字信号处理、现代通信原理、电子测量技术等。另外融入了专业最新前沿发展的课程，包括人工智能、移动通信、物联网、自动控制技术、云计算技术方向、移动技术等方向的课程，具体课程包括：《数字图像处理》、《微波技术与天线》、《物联网电路设计》、《人工智能及其应用》、《机器学习》、《智能信息处理》、《电子信息系统》等课程。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt">
+            <strong
+              ><span style="font-family: 仿宋; font-size: 16pt"
+                >五、实施效果</span
+              ></strong
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="text-align: left; line-height: 30pt; text-indent: 32pt"
+          >
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >以学生为中心，产出导向教育为抓手，构建了复合型、工程型、高素质电子信息技术本科人才培养模式。学生在各级各类电子学科专业竞赛中成绩斐然，相关教改项目获得了自治区级教学成果奖一等奖。成为立足广西、面向全国、辐射东盟的电子信息人才重要培养基地。毕业生在攻读研究生、出国深造等方面有突出表现，近<span
+                lang="EN-US"
+                >3</span
+              >年<span lang="EN-US">23</span
+              >名学生被“双一流”高校和国外高校录取。学生就业质量逐年提高，成为工作单位的骨干人才。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="text-align: left; line-height: 30pt; text-indent: 32pt"
+          >
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >近三年，本专业教师承担国家自然基金项目<span lang="EN-US">8</span
+              >项、广西创新驱动发展专项（科技重大专项）<span lang="EN-US"
+                >1</span
+              >项、广西自然科学基金<span lang="EN-US">3</span>项、发表<span
+                lang="EN-US"
+                >SCI/EI</span
+              >检索、中文核心期刊的科研论文<span lang="EN-US">35</span
+              >篇；获得广西自治区教学成果一等奖<span lang="EN-US">1</span
+              >项，获得自治区级虚拟仿真平台项目<span lang="EN-US">2</span
+              >项，教改项目<span lang="EN-US">2</span>项，出版教材<span
+                lang="EN-US"
+                >3</span
+              >部，建设慕课在线课程<span lang="EN-US">1</span>门。</span
+            >
+          </p>
+          <p
+            class="MsoNormal"
+            style="text-align: left; line-height: 30pt; text-indent: 32pt"
+          >
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >近三年，本专业学生获得国家级大学生创新创业实践项目<span
+                lang="EN-US"
+                >15</span
+              >项，自治区级大学生创新创业实践项目<span lang="EN-US">32</span
+              >项，获得计算机软件著作权登记证书<span lang="EN-US">64</span
+              >项、国际国内各类竞赛奖<span lang="EN-US">30</span
+              >项。为推动本专业创新创业教育高质量发展，贯彻落实《国务院办公厅关于进一步支持大学生创新创业的指导意见》（国办发（<span
+                lang="EN-US"
+                >2021)35</span
+              >号）精神，支持学生创办了科技企业<span lang="EN-US">1</span
+              >个，当年获得经济效益<span lang="EN-US">10</span
+              >万多元，有效提升了大学生创新创业能力。</span
+            >
+          </p>
+          <p class="MsoNormal" style="line-height: 30pt; text-indent: 32pt">
+            <span style="font-family: 仿宋; font-size: 16pt"
+              >本专业确立了符合社会需求和技术发展趋势的专业人才培养目标，专业教学和人才培养能紧紧围绕这一目标来进行，教学质量和人才培养质量得到进一步提高，得到了社会的肯定。从最近几年的招生情况来看，专业生源充足。毕业生的就业情况好，<span
+                lang="EN-US"
+                >2016</span
+              >年就业率为<span lang="EN-US">95.92%</span>；<span lang="EN-US"
+                >2017</span
+              >年就业率为<span lang="EN-US">97.62%</span>；<span lang="EN-US"
+                >2018</span
+              >年就业率为<span lang="EN-US">97.87%</span
+              >。到目前为止，本专业已培养毕业生<span lang="EN-US">2000</span
+              >多名。这些毕业生遍布广西区内外各地的电子、通信、计算机网络、广播电视、邮电等部门，以及政府机关和企事业单位。大多数毕业生能充分发挥所学知识和自己的聪明才智，在实际工作中取得了成绩，成为单位的技术领导和业务骨干。本院培养出来的电子信息工程专业的毕业生以其适应性强、专业基础比较扎实、有一定的创新精神和实际动手能力、吃苦耐劳等特点，普遍受到用人单位的好评。</span
+            >
+          </p>
+          <p class="MsoNormal" style="text-align: left; line-height: 30pt">
+            <span lang="EN-US" style="font-family: 仿宋; font-size: 16pt"
+              >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span
+            ><span style="font-family: 仿宋; font-size: 16pt"
+              >（引用数据截止<span lang="EN-US">2022</span>年<span lang="EN-US"
+                >6</span
+              >月）</span
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ElectronicInformation",
+};
+</script>
+
+<style scoped>
+.myDiv {
+  height: 760px;
+  overflow: auto;
+  width: 100%;
+  color: white;
+  background-color: #008bc2;
+  /* position: absolute;
+  top: 10%;
+  left: 15%; */
+}
+p {
+  text-align: left;
+  line-height: 40px;
+  font-family: 宋体;
+  font-size: 21px;
+}
+.con_con {
+  padding: 10px 20px 10px 20px;
+}
+p.MsoNormal,
+li.MsoNormal,
+div.MsoNormal {
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  text-align: justify;
+  text-justify: inter-ideograph;
+  font-size: 10.5pt;
+  font-family: "Calibri", sans-serif;
+}
+p.MsoHeader,
+li.MsoHeader,
+div.MsoHeader {
+  /* mso-style-link: "页眉 Char"; */
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  text-align: center;
+  /* vsb_temp-mode: char; */
+  border: none;
+  padding: 0cm;
+  font-size: 9pt;
+  font-family: "Calibri", sans-serif;
+}
+p.MsoFooter,
+li.MsoFooter,
+div.MsoFooter {
+  /* mso-style-link: "页脚 Char"; */
+  margin: 0cm;
+  margin-bottom: 0.0001pt;
+  /* vsb_temp-mode: char; */
+  font-size: 9pt;
+  font-family: "Calibri", sans-serif;
+}
+
+.MsoChpDefault {
+  font-family: "Calibri", sans-serif;
+} /* Page Definitions */
+@page WordSection1 {
+  size: 595.3pt 841.9pt;
+  margin: 72pt 90pt 72pt 90pt;
+  vsb_temp: 15.6pt;
+}
+</style>
